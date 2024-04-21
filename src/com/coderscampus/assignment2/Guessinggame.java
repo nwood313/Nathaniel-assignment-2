d43005a5979ff40cbf4995937a23df677896c9ac
nwood313 <161774016+nwood313@users.noreply.github.com>
2024-04-21 15:54:11 -0400

    compiled
    I finally got it to ignore guesses outside of  range

--- src/com/coderscampus/assignment2/Guessinggame.java
+++ src/com/coderscampus/assignment2/Guessinggame.java
@@ -9,19 +9,36 @@ public class Guessinggame {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        System.out.println("Enter a number between 1 & 100");
-        String number = scanner.nextLine();
-        System.out.println("Type in Your Age");
-        String age = scanner.nextLine();
-        if (random.equals("Michael Jacobs")
-                || age .equals("32")) {
-            System.out.println("Why are you weird?");
-            System.out.println("You are weird!");
-        }   else
-        {System.out.println("Hello:" + number);
+        int attempts = 5; // Number of allowed attempts
+        int random1 = random.nextInt(100+1);
+
+        System.out.println("Welcome to the Guessing Game! Pick a number between 1 and 100.");
+
+        for (int i = 0; i < attempts; i++) {
+            System.out.print("Enter your guess: ");
+            int userInput = scanner.nextInt();
+
+            if (userInput < random1) {
+                System.out.println("Please pick a HIGHER number");
+            } else if (userInput > random1) {
+                System.out.println("Please pick a LOWER number");
+            } else {
+                System.out.println("You are the Winner!");
+                break;
+            }
+            if (userInput < 0 || userInput > 100)  {
+                System.out.println("Oops, that number wasn't between 1 & 100 try again");
+                System.out.println("Try again! You have " + (attempts - i - 0) + " attempts left.");
+                continue;
+            }
+                if (i < attempts - 1) {
+                System.out.println("Try again! You have " + (attempts - i - 1) + " attempts left.");
+            } else {
+                System.out.println("Oh Brother Doug, WHAT A LOSER! . The correct number was " + random1 + ".");
+                    System.out.println("GAME OVER!");
+            }
+
         }
-
-
     }
 
 }
