1bf68fb1e6f4e9ffadc7983f0c445035275fafc0
nwood313 <161774016+nwood313@users.noreply.github.com>
2024-08-02 19:28:06 -0400

    Update Guessinggame.java

--- src/com/coderscampus/assignment2/Guessinggame.java
+++ src/com/coderscampus/assignment2/Guessinggame.java
@@ -10,31 +10,33 @@ public class Guessinggame {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int attempts = 5;
-        int randomNumber = random.nextInt(100) + 1;
-
+        int randomNumber = random.nextInt(1,101) ;
+        int attemptsCtr = 0;
         System.out.println("Welcome to the Guessing Game! Pick a number between 1 and 100.");
         //System.out.println(randomNumber); // my way to test winning conditions
 
-        for (int i = 0; i < attempts; i++) {
+        while (attemptsCtr < attempts) {
             System.out.print("Enter your guess: ");
             int yourGuess = scanner.nextInt();
 
-            while (yourGuess < 1 || yourGuess > 100) {
+            if (yourGuess < 1 || yourGuess > 100) {
                 System.out.println("WHOOPSIE, that number wasn't between 1 & 100. Try again!");
-                yourGuess = scanner.nextInt();
+                continue;
             }
+            attemptsCtr++;
 
-            if (yourGuess < randomNumber) {
-                System.out.println("Please pick a HIGHER number.");
-            } else if (yourGuess > randomNumber) {
-                System.out.println("Please pick a LOWER number.");
-            } else {
+            if (yourGuess == randomNumber) {
                 System.out.println("Winner, Winner, Chicken Dinner!");
                 break;
+            } else if (yourGuess < randomNumber){
+                System.out.println("Please pick a HIGHER number.");
+
+            } else if (yourGuess > randomNumber) {
+                System.out.println("Please pick a LOWER number.");
             }
 
-            if (i < attempts - 1) {
-                System.out.println("Try again! You have " + (attempts - i - 1) + " attempts left.");
+            if ( attemptsCtr < attempts) {
+                System.out.println("Try again! You have " + (attempts - attemptsCtr) + " attempts left.");
             } else {
                 System.out.println("Oh Brother, WHAT A LOO-SER! The correct number was " + randomNumber + ".");
                 System.out.println("GAME OVER!");
